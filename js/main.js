b07e4f1093484783da636cedf740ff2571eb3b57
Tbenipal <156356265+Tbenipal@users.noreply.github.com>
2024-06-11 23:32:19 -0400

    Update main.js
    added semi column

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const SVG = document.querySelectorAll("#SVG svg");
 console.log(SVG);
 
 function logId () {
-    console.log(this.id)
+    console.log(this.id);
 }
 
 SVG.forEach((icons) => icons.addEventListener("click", logId));
